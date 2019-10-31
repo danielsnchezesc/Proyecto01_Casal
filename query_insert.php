518f3f6ffbe0bff19c400b2c791aec113f932730
José Antonio Nuevo Claverías <50334452+JoseAntonioNuevo@users.noreply.github.com>
2019-10-31 15:55:01 +0100

    Añadir Update

--- query_insert.php
+++ query_insert.php
@@ -1,5 +1,9 @@
 <?php
+//Insertar los datos del pedido
 $sql = "INSERT INTO pedidos (nombre_Pedidos, fecha_inicio_Pedidos, hora_inicio_Pedidos, inventario_Pedidos, personal_Pedidos) VALUES 
 ('$nombre', '$fecha', '$hora', '$inventario', '$usuario')";
 mysqli_query($conexion, $sql);
+
+//Marca un producto como reservado
+$x = "UPDATE inventario SET reservado_Inventario='1' WHERE nombre_Inventario='$inventario'";
 ?>
