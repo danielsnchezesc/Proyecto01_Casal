36c778352dcd8151a305358b78a596c4255442c4
CarlosMunozDeLaPena <55807969+CarlosMunozDeLaPena@users.noreply.github.com>
2019-10-31 17:20:01 +0100

    Add files via upload
    login con css

--- login.php
+++ login.php
@@ -2,20 +2,28 @@
 <html>
 <head>
 	<title>Inventario casal</title>
-	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<link rel="stylesheet" type="text/css" href="login.css">
 	<meta charset="UTF-8">
 	<script type="text/javascript" src="javascript.js"></script>
 </head>
 <body>
-	<h2>Insertar usuarios</h2>
-	<p class="mensaje" id="mensaje"></p>
-	<form action="comprobarusuarios.php" method="POST" name="formulario1" enctype="multipart/form-data" onsubmit="return login()">
-		<label for="user" class="boton">Usuario:</label>
-		<input type="string" name="user" id="user" default=""><br><br>
-		<label for="pwd" class="boton">Contraseña:</label>
-		<input type="password" name="pwd" id="pwd">
-		<input type="submit" name="registro" default="" class="boton">
-	</form>
+
+	<div class="contenedor">
+		<div class="is">
+		<h3>Iniciar Sesión</h3>
+		</div><br>
+
+		<div class="form">
+		<form action="comprobarusuarios.php" method="POST" name="formulario1" enctype="multipart/form-data" onsubmit="return login()">
+			<label for="user" class="boton">Usuario:</label><br>
+			<input type="string" name="user" id="user" default=""><br><br>
+			<label for="pwd" class="boton">Contraseña:</label><br>
+			<input type="password" name="pwd" id="pwd"><br>
+			<input  class="btnenviar" type="submit" name="registro" default="" value="Entrar">
+		</form>
+		</div>
+	</div>
+
 </div>
 </body>
 </html>
