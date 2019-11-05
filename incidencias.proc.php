b102ae0ddfb5a90fb0f19f3379fea2f47211dfd2
José Antonio Nuevo Claverías <50334452+JoseAntonioNuevo@users.noreply.github.com>
2019-11-05 17:10:11 +0100

    añadir update descripcion

--- incidencias.proc.php
+++ incidencias.proc.php
@@ -1,11 +1,11 @@
 <?php
 include "conexion.php";
 $id=$_REQUEST['opcion'];
-//Marcar un producto como estropeado
-$y = "UPDATE inventario SET estado_Inventario='3' WHERE id_inventario='$id'";
-mysqli_query($conn, $y);
+$descripcion=$_REQUEST['descripcion_estado'];
 
-//header("Location: incidencias.php");
+//Marcar un producto como estropeado
+$y = "UPDATE inventario SET estado_Inventario='3' AND descripcion_estado_Inventario='$descripcion' WHERE id_inventario='$id'";
+mysqli_query($conn, $y);
 ?>
 
 <p>Se ha marcado como estropeado</p>
